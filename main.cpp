6d59d14a1a60ea31d638bc71b11a479d2512d6b5
joewanchen <cx282694@gmail.com>
2016-08-06 16:17:13 +0800

    add files.

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+//#include "CryCommon/CryModuleDefs.h"
+//#define eCryModule eCryM_Game
+
+//#include <platform.h>
+//#include "CryCommon/ISystem.h"
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+
+    //IScriptSystem* ptr = nullptr;
+    //ptr->DebugContinue();
+
+    //ISystem* ptr2 = nullptr;
+    //ptr2->CreateAVIReader();
+
+    return 0;
+}
